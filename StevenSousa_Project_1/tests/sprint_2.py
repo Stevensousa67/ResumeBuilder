87e6e0a873724fac3fac45f0e3b1eab9ad0ba62e
Steven Sousa <silver.steven67@gmail.com>
2025-02-10 11:03:14 -0500

    Created placeholder test file

---
+++ StevenSousa_Project_1/tests/sprint_2.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_get_file_data(self):
+        """
+        TODO: Write a test that captures data from file
+        :return:
+        """
+
+
+    def test_db(self):
+        """
+        TODO: Create a test that creates a database, inserts data, and check if data is there.
+        :return:
+        """
+
+
+if __name__ == '__main__':
+    unittest.main()
