c2ad8a70182e6554fcfe196cb4d9ce4d37c7b740
Steven Sousa <silver.steven67@gmail.com>
2025-02-11 07:46:47 -0500

    added code to create user, database, and assign ownership to database, and change connection

--- StevenSousa_Project_1/src/main.py
+++ StevenSousa_Project_1/src/main.py
@@ -31,11 +31,28 @@ def main():
     # Load environment variables
     dotenv.load_dotenv(os.path.join(os.path.dirname(os.getcwd()), '.env'))
 
-    # Create DB Connection
+    # Connect using PostgresSQL default values
+    conn_default, cursor_default = DBUtils.open_db(os.getenv('DB_NAME_DEFAULT'), os.getenv('DB_USER_DEFAULT'),
+                                           os.getenv('DB_PASSWORD_DEFAULT'), os.getenv('DB_HOST'),
+                                                   os.getenv('DB_PORT'), autocommit=True)
+
+    # Create custom user
+    DBUtils.create_user(cursor_default, os.getenv('DB_USER'), os.getenv('DB_PASSWORD'))
+
+    # Create custom database
+    DBUtils.create_db(cursor_default, os.getenv('DB_NAME'))
+
+    # Assign newly created database to new user
+    DBUtils.assign_ownership(cursor_default, os.getenv('DB_NAME'), os.getenv('DB_USER'))
+
+    # Close existing connection in order to change databases
+    DBUtils.close_db(conn_default, cursor_default)
+
+    # Start new database connection using new
     conn, cursor = DBUtils.open_db(os.getenv('DB_NAME'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD'),
-                                   os.getenv('DB_HOST'), os.getenv('DB_PORT'))
+                                   os.getenv('DB_HOST'), os.getenv('DB_PORT'), autocommit=False)
     # Create table in DB
-    DBUtils.setup_db(conn, cursor)
+    DBUtils.setup_table(conn, cursor)
 
     # Process data
     Data_Processing.process_json('../data/rapidResults.json', conn, cursor)
