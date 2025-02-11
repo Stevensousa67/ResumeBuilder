c9df646b2f5a91c67093c32ab0547e28e3372f5d
Steven Sousa <silver.steven67@gmail.com>
2025-02-11 07:46:03 -0500

    created methods to create user, database, and assign database ownership to user

--- StevenSousa_Project_1/src/DBUtils.py
+++ StevenSousa_Project_1/src/DBUtils.py
@@ -11,7 +11,7 @@ from typing import Tuple
 from psycopg import DatabaseError, Connection, Cursor, connect, sql
 
 
-def open_db(dbname: str, user: str, password: str, host: str, port: str) -> Tuple[Connection, Cursor]:
+def open_db(dbname: str, user: str, password: str, host: str, port: str, autocommit: bool = False) -> Tuple[Connection, Cursor]:
     """
     This function will open a connection to the PostgreSQL database.
     :param dbname: os.getenv('DB_NAME')
@@ -19,6 +19,7 @@ def open_db(dbname: str, user: str, password: str, host: str, port: str) -> Tupl
     :param password: os.getenv('DB_PASSWORD')
     :param host: os.getenv('DB_HOST')
     :param port: os.getenv('DB_PORT')
+    :param autocommit: True if you want to save changes automatically
     :return: connection and cursor to the database
     """
     try:
@@ -29,11 +30,63 @@ def open_db(dbname: str, user: str, password: str, host: str, port: str) -> Tupl
             host=host,
             port=port
         )
+        conn.autocommit = autocommit
         cursor = conn.cursor()
         return conn, cursor
+
     except (Exception, DatabaseError) as error:
         print(error)
         raise error
+
+
+def create_user(cursor: Cursor, username: str, password: str):
+    """
+    This function will create a user in the PostgreSQL server
+    :param cursor: cursor to the server
+    :param username: username for the server
+    :param password: password for the username
+    :return:
+    """
+    # Create the user, if it doesn't exist
+    cursor.execute(sql.SQL("SELECT 1 FROM pg_roles WHERE rolname = %s"), (username,))
+    user_exists = cursor.fetchone()
+
+    if not user_exists:
+        cursor.execute(sql.SQL("CREATE USER {} WITH PASSWORD %S").format(sql.Identifier(username)), (password,))
+        print(f'User {username} created successfully.')
+    else:
+        print(f'User {username} already exists.')
+
+
+def create_db(cursor: Cursor, db_name):
+    """
+    This function will create a database in the PostgreSQL server
+    :param cursor: cursor to the server
+    :param db_name: name of the database
+    :return:
+    """
+
+    cursor.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), (db_name,))
+    db_exists = cursor.fetchone()
+
+    if not db_exists:
+        cursor.execute(sql.SQL("CREATE DATABASE %s"), (db_name,))
+        print(f'Database {db_name} created successfully.')
+    else:
+        print(f'Database {db_name} already exists.')
+
+
+def assign_ownership(cursor: Cursor, db_name: str, username: str):
+    """
+    This function will assign ownership of a database to the specified user.
+    :param cursor: cursor to the server
+    :param db_name: database name
+    :param username: owner username
+    :return:
+    """
+
+    cursor.execute(sql.SQL("ALTER DATABASE {} OWNER TO {}").format(sql.Identifier(db_name),
+                                                                   sql.Identifier(username)))
 
 
 def close_db(conn: Connection, cursor: Cursor) -> None:
@@ -48,7 +101,7 @@ def close_db(conn: Connection, cursor: Cursor) -> None:
     conn.close()
 
 
-def setup_db(conn: Connection, cursor: Cursor) -> None:
+def setup_table(conn: Connection, cursor: Cursor) -> None:
     """
     This function will create the jobs table if it doesn't already exist
     :param cursor: connection to the database
@@ -62,8 +115,8 @@ def setup_db(conn: Connection, cursor: Cursor) -> None:
         company_name TEXT NOT NULL,
         job_description TEXT NOT NULL,
         location TEXT NOT NULL,
-        min_salary INT DEFAULT 0,
-        max_salary INT DEFAULT 0,
+        min_salary DECIMAL DEFAULT 0,
+        max_salary DECIMAL DEFAULT 0,
         salary_time TEXT DEFAULT 'yearly',
         posted_date TEXT,
         url TEXT NOT NULL,
@@ -96,23 +149,17 @@ def drop_table(conn: Connection, cursor: Cursor) -> None:
     :param conn: connection to the database
     :return: None
     """
-    cursor.execute(sql.SQL(
-        """DROP TABLE IF EXISTS jobs
-        RESTRICT;"""
-    ))
+    cursor.execute(sql.SQL("""DROP TABLE IF EXISTS jobs RESTRICT;"""))
     conn.commit()
     cursor.close()
     conn.close()
 
 
-def retrieve_job(conn: Connection, cursor: Cursor) -> Tuple:
+def retrieve_job(cursor: Cursor) -> Tuple:
     """
-    This function will retrieve the a job from the DB.
+    This function will retrieve a job from the DB.
     :param cursor: cursor to the database
-    :param conn: connection to the database
     :return: job
     """
-    cursor.execute(sql.SQL(
-        """SELECT * FROM jobs WHERE job_id = %s"""
-    ))
+    cursor.execute(sql.SQL("""SELECT * FROM jobs WHERE job_id = %s"""))
     return cursor.fetchone()
