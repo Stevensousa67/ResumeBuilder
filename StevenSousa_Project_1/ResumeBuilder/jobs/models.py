65cde75c393cac8383d712d0777dffc05185e8f9
Steven Sousa <silver.steven67@gmail.com>
2025-02-24 21:20:54 -0500

    create correct table based on prod or test environment
    Signed-off-by: Steven Sousa <silver.steven67@gmail.com>

--- StevenSousa_Project_1/ResumeBuilder/jobs/models.py
+++ StevenSousa_Project_1/ResumeBuilder/jobs/models.py
@@ -16,7 +16,7 @@ class Job(models.Model):
     remote = models.BooleanField(default=False)
 
     class Meta:
-       db_table = 'django_jobs' if getattr(settings, 'TESTING', False) else 'test_django_jobs'
+        db_table = 'django_jobs' if not settings.TESTING else 'test_django_jobs'
 
     def __str__(self):
         return self.job_title
