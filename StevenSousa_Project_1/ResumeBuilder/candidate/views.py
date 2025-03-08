14a458681cf9b30edc42c78cf2c6507516657380
Steven Sousa <silver.steven67@gmail.com>
2025-03-07 22:00:33 -0500

    removed unused imports

--- StevenSousa_Project_1/ResumeBuilder/candidate/views.py
+++ StevenSousa_Project_1/ResumeBuilder/candidate/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 from formtools.wizard.views import SessionWizardView
-from django.forms.widgets import Select
 from .forms import SignupForm, UserForm, ProfileForm, ProfileSelectForm, ExperienceFormSet, ProjectFormSet, \
     ReferenceFormSet
 from .models import User, Profile, Experience, Project, Reference
 from django.contrib.auth.views import LoginView
-from django.core.exceptions import ObjectDoesNotExist
 
 
 # Custom Login View
